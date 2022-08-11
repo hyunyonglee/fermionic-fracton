cc4bd47913a0fb94934b19ba33f72bb6138af3dc
hyunyonglee <hyunyong.rhee@gmail.com>
2022-08-11 22:08:48 +0900

    revise

--- model.py
+++ model.py
@@ -5,107 +5,43 @@ from tenpy.models.lattice import Site, Chain, Square
 from tenpy.models.model import CouplingModel, NearestNeighborModel, MPOModel, CouplingMPOModel
 from tenpy.linalg import np_conserved as npc
 from tenpy.tools.params import Config
-from tenpy.networks.site import BosonSite  # if you want to use the predefined site
+from tenpy.networks.site import FermionSite  # if you want to use the predefined site
 import matplotlib.pyplot as plt
-__all__ = ['DIPOLAR_BOSE_HUBBARD']
+__all__ = ['FERMIONIC_FRACTON']
 
 
-class DIPOLAR_BOSE_HUBBARD(CouplingModel,MPOModel):
+class FERMIONIC_FRACTON(CouplingModel,MPOModel):
     
     def __init__(self, model_params):
         
         # 0) read out/set default parameters
         if not isinstance(model_params, Config):
-            model_params = Config(model_params, "DIPOLAR_BOSE_HUBBARD")
-        L = model_params.get('L', 1)
-        t = model_params.get('t', 1.)
-        tp = model_params.get('tp', 1.)
-        U = model_params.get('U', 1.)
-        Ut = model_params.get('Ut', 0.)
-        mu = model_params.get('mu', 0.)
-        Ncut = model_params.get('Ncut', 2)
-        bc_MPS = model_params.get('bc_MPS', 'infinite')
-        bc = model_params.get('bc', 'periodic')
-        QN = model_params.get('QN', 'N')
-
-        site = BosonSite( Nmax=Ncut, conserve=QN, filling=0.0 )
-        site.multiply_operators(['B','B'])
-        site.multiply_operators(['Bd','Bd'])
-
-        # MPS boundary condition
-        if bc_MPS == 'finite' and bc == 'periodic':
-            order = 'folded'
-        else:
-            order = 'default'
-        
-        lat = Chain( L=L, site=site, bc=bc, bc_MPS=bc_MPS, order=order )
-        CouplingModel.__init__(self, lat)
-
-        # NN Hubbard Interaction
-        self.add_coupling( Ut, 0, 'N', 0, 'N', 1)
-        
-        # 3-site hopping
-        self.add_multi_coupling( -t, [('Bd', 0, 0), ('B B', 1, 0), ('Bd', 2, 0)])
-        self.add_multi_coupling( -t, [('B', 0, 0), ('Bd Bd', 1, 0), ('B', 2, 0)])
-
-        # 4-site hopping
-        self.add_multi_coupling( -tp, [('Bd', 0, 0), ('B', 1, 0), ('B', 2, 0), ('Bd', 3, 0)])
-        self.add_multi_coupling( -tp, [('B', 0, 0), ('Bd', 1, 0), ('Bd', 2, 0), ('B', 3, 0)])
-
-        # hubbard
-        self.add_onsite( U/2., 0, 'NN')
-
-        # chemical potential
-        self.add_onsite( -( mu + U/2. ), 0, 'N')
-
-        
-        MPOModel.__init__(self, lat, self.calc_H_MPO())
-
-
-
-class DIPOLAR_BOSE_HUBBARD_2D(CouplingModel,MPOModel):
-    
-    def __init__(self, model_params):
-        
-        # 0) read out/set default parameters
-        if not isinstance(model_params, Config):
-            model_params = Config(model_params, "DIPOLAR_BOSE_HUBBARD")
+            model_params = Config(model_params, "FERMIONIC_FRACTON")
         Lx = model_params.get('Lx', 1)
         Ly = model_params.get('Ly', 1)
         t = model_params.get('t', 1.)
-        tp = model_params.get('tp', 1.)
         U = model_params.get('U', 1.)
         mu = model_params.get('mu', 0.)
-        Ncut = model_params.get('Ncut', 2)
         bc_MPS = model_params.get('bc_MPS', 'infinite')
         bc = model_params.get('bc', 'periodic')
         QN = model_params.get('QN', 'N')
 
-        site = BosonSite( Nmax=Ncut, conserve=QN, filling=0.0 )
-        site.multiply_operators(['B','B'])
-        site.multiply_operators(['Bd','Bd'])
-
+        site = FermionSite( conserve=QN, filling=0.5 )
+        
         lat = Square(Lx=Lx, Ly=Ly, site=site, bc=bc, bc_MPS=bc_MPS)
         CouplingModel.__init__(self, lat)
         
-        # 3-site hopping
-        if np.abs(t) > 1.0e-8:
-            self.add_multi_coupling( -t, [('Bd', [0,0], 0), ('B B', [1,0], 0), ('Bd', [2,0], 0)])
-            self.add_multi_coupling( -t, [('B', [0,0], 0), ('Bd Bd', [1,0], 0), ('B', [2,0], 0)])
-            self.add_multi_coupling( -t, [('Bd', [0,0], 0), ('B B', [0,1], 0), ('Bd', [0,2], 0)])
-            self.add_multi_coupling( -t, [('B', [0,0], 0), ('Bd Bd', [0,1], 0), ('B', [0,2], 0)])
-        
         # 4-site hopping
-        self.add_multi_coupling( -tp, [('Bd', [0,0], 0), ('B', [1,0], 0), ('B', [0,1], 0), ('Bd', [1,1], 0)])
-        self.add_multi_coupling( -tp, [('B', [0,0], 0), ('Bd', [1,0], 0), ('Bd', [0,1], 0), ('B', [1,1], 0)])
-
-        # hubbard
-        self.add_onsite( U/2., 0, 'NN')
-
-        # chemical potential
-        self.add_onsite( -( mu + U/2. ), 0, 'N')
-
+        self.add_multi_coupling( U, [('Cd', [1,1], 0), ('Cd', [0,0], 0), ('C', [1,0], 0), ('C', [0,1], 0)])
+        self.add_multi_coupling( U, [('Cd', [0,1], 0), ('Cd', [1,0], 0), ('C', [0,0], 0) ,('C', [1,1], 0)])
         
+        # chemical potential
+        self.add_onsite( -mu, 0, 'N')
+
+        # hopping
+        for u1, u2, dx in self.lat.pairs['nearest_neighbors']:
+            self.add_coupling( -t, u1, 'Cd', u2, 'C', dx, plus_hc=True)
+          
         MPOModel.__init__(self, lat, self.calc_H_MPO())
 
         
--- run_model.py
+++ run_model.py
@@ -37,106 +37,56 @@ logging.config.dictConfig(conf)
 
 # os.environ["OMP_NUM_THREADS"] = "68"
 
-L = int(sys.argv[1])
-t = float(sys.argv[2])
-tp = float(sys.argv[3])
+Lx = int(sys.argv[1])
+Ly = int(sys.argv[2])
+t = float(sys.argv[3])
 U = float(sys.argv[4])
-Ut = float(sys.argv[5])
-mu = float(sys.argv[6])
-Ncut = int(sys.argv[7])
-CHI = int(sys.argv[8])
-RM = sys.argv[9]
-QN = sys.argv[10]
-PATH = sys.argv[11]
-BC_MPS = sys.argv[12]
-BC = sys.argv[13]
-IS = sys.argv[14]
+mu = float(sys.argv[5])
+CHI = int(sys.argv[6])
+RM = sys.argv[7]
+QN = sys.argv[8]
+PATH = sys.argv[9]
+BC_MPS = sys.argv[10]
+IS = sys.argv[11]
+
+if BC_MPS == 'infinite':
+    BC = 'periodic'
+else:
+    BC = ['periodic','open']
 
 model_params = {
-    "L": L,
+    "Lx": Lx,
+    "Ly": Ly,
     "t": t,
-    "tp": tp,
     "U": U,
-    "Ut": Ut,
     "mu": mu,
-    "Ncut": Ncut,
     "bc_MPS": BC_MPS,
     "bc": BC,
     "QN": QN
 }
 
 print("\n\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-M = model.DIPOLAR_BOSE_HUBBARD(model_params)
+M = model.FERMIONIC_FRACTON(model_params)
 
+L = Lx * Ly
 # initial state
-if IS == '1-1p':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '2'
-
-elif IS == '1-2p':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '2'
-    product_state[int(L/2)+1] = '2'
-    
-elif IS == '1-1h':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '0'
-    
-elif IS == '1-2h':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[int(L/2)] = '0'
-    product_state[int(L/2)+1] = '0'
-
-elif IS == 'half':
-    product_state = ['1','0'] * int(M.lat.N_sites/2)
-    
-elif IS == 'half-1p':
-    product_state = ['1','0'] * int(M.lat.N_sites/2)
-    product_state[int(M.lat.N_sites/2)+1] = '1'
-
-elif IS == 'half-1h':
-    product_state = ['1','0'] * int(M.lat.N_sites/2)
-    product_state[int(M.lat.N_sites/2)] = '0'
-
-elif IS == '0.9':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[0] = '0'
-
-elif IS == '0.8':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[0] = '0'
-    product_state[1] = '0'
-
-elif IS == '0.7':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[0] = '0'
-    product_state[1] = '0'
-    product_state[2] = '0'
-
-elif IS == '0.6':
-    product_state = ['1'] * M.lat.N_sites
-    product_state[0] = '0'
-    product_state[1] = '0'
-    product_state[2] = '0'
-    product_state[3] = '0'
+if IS == 'checkerboard':
+    product_state = ( ['empty', 'full'] * int(Ly/2) + ['full', 'empty'] * int(Ly/2) ) * int(Lx/2)
+    print(product_state)
 
 else:
     product_state = [IS] * M.lat.N_sites
     
 psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
 
-
 if RM == 'random':
-    TEBD_params = {'N_steps': 4, 'trunc_params':{'chi_max': 4}, 'verbose': 0}
+    TEBD_params = {'N_steps': 20, 'trunc_params':{'chi_max': 32}, 'verbose': 0}
     eng = tebd.RandomUnitaryEvolution(psi, TEBD_params)
     eng.run()
     psi.canonical_form() 
 
-# dchi = int(CHI/5)
-# chi_list = {0: 8, 10: 16, 20: 32, 30: CHI}
+
 chi_list = {0: 4, 4: 8, 8: 16, 12: 32, 16: 64, 20: CHI}
-# for i in range(5):
-#     chi_list[i*20] = (i+1)*dchi
 
 if BC_MPS == 'infinite':
     max_sweep = 1000
@@ -148,7 +98,6 @@ else:
     S_err = 1.0e-8
 
 dmrg_params = {
-    # 'mixer': True,  # setting this to True helps to escape local minima
     'mixer' : dmrg.SubspaceExpansion,
     'mixer_params': {
         'amplitude': 1.e-3,
@@ -159,10 +108,6 @@ dmrg_params = {
         'chi_max': CHI,
         'svd_min': 1.e-9
     },
-    # 'lanczos_params': {
-    #         'N_min': 5,
-    #         'N_max': 20
-    # },
     'chi_list': chi_list,
     'max_E_err': 1.0e-8,
     'max_S_err': S_err,
@@ -177,64 +122,40 @@ ensure_dir(PATH + "mps/")
 
 # ground state
 eng = dmrg.TwoSiteDMRGEngine(psi, M, dmrg_params)
-# eng = dmrg.SingleSiteDMRGEngine(psi, M, dmrg_params)
 E, psi = eng.run()  # equivalent to dmrg.run() up to the return parameters.
 
 N = psi.expectation_value("N")
-B = np.abs( psi.expectation_value("B") )
+C = np.abs( psi.expectation_value("C") )
 EE = psi.entanglement_entropy()
 ES = psi.entanglement_spectrum()
 
 if BC_MPS == 'finite':
-    R = L-1
+    lx = Lx-1
     xi = 0.
 else:
-    R = L
+    lx = Lx
     xi = psi.correlation_length()
-
-# measuring exciton condensation
-hs = []
-if BC_MPS == 'finite' and BC == 'periodic':
-    for i in range(0,int(L/2-1)): 
-        I = 2*i
-        hs.append( np.abs( psi.expectation_value_term([('Bd',I+2),('B',I)]) ) )
-    hs.append( np.abs( psi.expectation_value_term([('Bd',L-1),('B',L-2 )]) ) )
-    for i in range(0,int(L/2-1)):
-        I = L-1 - 2*i
-        hs.append( np.abs( psi.expectation_value_term([('Bd',I-2),('B',I)]) ) )
-    hs.append( np.abs( psi.expectation_value_term([('Bd',0),('B',1)]) ) )
-
-else:
-    for i in range(0,R): 
-        hs.append( np.abs( psi.expectation_value_term([('Bd',i+1),('B',i)]) ) )
-#
 
 
 file1 = open( PATH + "observables/energy.txt","a")
-file1.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + repr(E) + " " + repr( np.mean(N) ) + " " + repr( np.mean(B) ) + " " + repr( np.mean(hs) ) + " " + repr(xi) + " " + "\n")
+file1.write(repr(t) + " " + repr(U) + " " + repr(mu) + " " + repr(E) + " " + repr( np.mean(N) ) + " " + repr( np.mean(C) ) + " " + repr(xi) + " " + "\n")
 
 file2 = open( PATH + "observables/numbers.txt","a")
-file2.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, N)) + " " + "\n")
+file2.write(repr(t) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, N)) + " " + "\n")
 
-file3 = open( PATH + "observables/exciton_density.txt","a")
-file3.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, hs)) + " " + "\n")
+file3 = open( PATH + "observables/condensation.txt","a")
+file3.write(repr(t) + " " + repr(U) + " " + repr(mu) + " " + "  ".join(map(str, C)) + " " + "\n")
 
-file4 = open( PATH + "observables/condensation.txt","a")
-file4.write(repr(t) + " " + repr(tp) + " " + repr(U) + " " + repr(Ut) + " " + repr(mu) + " " + "  ".join(map(str, B)) + " " + "\n")
-
-file_ES = open( PATH + "entanglement/es_t_%.2f_tp_%.2f_U_%.2f_Ut_%.2f_mu_%.2f.txt" % (t,tp,U,Ut,mu),"a")
-for i in range(0,R):
-    file_ES.write("  ".join(map(str, ES[i])) + " " + "\n")
-file_EE = open( PATH + "entanglement/ee_t_%.2f_tp_%.2f_U_%.2f_Ut_%.2f_mu_%.2f.txt" % (t,tp,U,Ut,mu),"a")
+file_EE = open( PATH + "entanglement/ee_t_%.2f_U_%.2f_mu_%.2f.txt" % (t,U,mu),"a")
 file_EE.write("  ".join(map(str, EE)) + " " + "\n")
 
-file_STAT = open( PATH + "logs/stat_t_%.2f_tp_%.2f_U_%.2f_Ut_%.2f_mu_%.2f.txt" % (t,tp,U,Ut,mu),"a")
+file_STAT = open( PATH + "logs/stat_t_%.2f_U_%.2f_mu_%.2f.txt" % (t,U,mu),"a")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['E'])) + " " + "\n")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['S'])) + " " + "\n")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['max_trunc_err'])) + " " + "\n")
 file_STAT.write("  ".join(map(str,eng.sweep_stats['norm_err'])) + " " + "\n")
 
-with open( PATH + 'mps/gs_t_%.2f_tp_%.2f_U%.2f_Ut%.2f_mu%.2f.pkl' % (t,tp,U,Ut,mu), 'wb') as f:
+with open( PATH + 'mps/gs_t_%.2f_U%.2f_mu%.2f.pkl' % (t,U,mu), 'wb') as f:
     pickle.dump(psi, f)
 
 
